ea15bd2af0348893bbc1762716a7a3d194978bce
Mark Cook <markus.koch@photomaster.at>
2012-10-07 21:27:02 +0200

    First Test

---
+++ DesktopOverlay/DesktopOverlay/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Xml
+
+Public Class Form1
+
+    Dim drag As Boolean
+    Dim mouseX As Integer
+    Dim mouseY As Integer
+    Dim ConfigPath As String = Application.StartupPath & "\config.xml"
+    Dim ConfigData As New XmlDocument
+    Dim ConfigNode As XmlNode
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ConfigData.Load(ConfigPath)
+        ConfigNode = ConfigData.SelectSingleNode("/DesktopOverlay")
+        PictureBox1.ImageLocation = ConfigNode.SelectSingleNode("Imagepath").InnerText
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As System.Object, e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If (e.Control And e.Alt And e.KeyCode = Keys.F) Then 'STRG + ALT + F
+            Me.WindowState = FormWindowState.Maximized
+        ElseIf (e.Control And e.Alt And e.KeyCode = Keys.N) Then 'STRG + ALT + N
+            Me.WindowState = FormWindowState.Normal
+        ElseIf (e.Control And e.Alt And e.KeyCode = Keys.Q) Then 'STRG + ALT + Q
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseDown
+        drag = True
+        mouseX = Windows.Forms.Cursor.Position.X - Me.Left
+        mouseY = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mouseY
+            Me.Left = Windows.Forms.Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseUp
+        drag = False
+    End Sub
+
+End Class
---
+++ DesktopOverlay/DesktopOverlay/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("DesktopOverlay")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("DesktopOverlay")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012 M.Koch")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("477c4c38-fff3-4ed0-b7fe-cde4c98600fa")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
